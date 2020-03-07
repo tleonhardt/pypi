7e16bb0e8c16687ff76c66b4121d6278a653ca2d
Todd Leonhardt <todd.leonhardt@gmail.com>
2020-03-06 21:08:34 -0500

    Added an integer constrained route

--- pypi_org/views/package_views.py
+++ pypi_org/views/package_views.py
@@ -9,5 +9,11 @@ blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 @blueprint.route('/project/<package_name>')
 # @response(template_file='packages/details.html')
 def package_details(package_name: str):
-    """Homepage."""
+    """Get details for a particular package."""
     return "Package details for {}".format(package_name)
+
+
+@blueprint.route('/<int:rank>')
+def popular(rank: int):
+    """Get details for the nth most popular package."""
+    return "The details for the {}th most popular package".format(rank)
