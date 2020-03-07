4f1c95f7db9cc165240ec45ac2720591a0a18524
Todd Leonhardt <todd.leonhardt@gmail.com>
2020-03-07 10:41:27 -0500

    Added the rest of the tables

--- pypi_org/data/package.py
+++ pypi_org/data/package.py
@@ -1,12 +1,13 @@
 import datetime
+from typing import List
 
 import sqlalchemy as sa
-
+import sqlalchemy.orm as orm
 from pypi_org.data.modelbase import SqlAlchemyBase
+from pypi_org.data.releases import Release
 
 
 class Package(SqlAlchemyBase):
-    """SQLAlchemy model for packages."""
     __tablename__ = 'packages'
 
     id = sa.Column(sa.String, primary_key=True)
@@ -23,8 +24,20 @@ class Package(SqlAlchemyBase):
 
     license = sa.Column(sa.String, index=True)
 
-    # maintainers
-    # releases
+    # releases relationship
+    releases: List[Release] = orm.relation("Release", order_by=[
+        Release.major_ver.desc(),
+        Release.minor_ver.desc(),
+        Release.build_ver.desc(),
+    ], back_populates='package')
 
     def __repr__(self):
         return '<Package {}>'.format(self.id)
+
+
+# p = Package()  # one query
+#
+# print(p.id)
+# print("All releases")
+# for r in p.releases:
+#     print("{}.{}.{}".format(r.major_ver, r.minor_ver, r.build_ver))
